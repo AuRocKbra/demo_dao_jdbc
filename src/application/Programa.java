3d50736920e343e7b8b11772533fe57654a36262
AuRocKbra <4haurock@gamil.com>
2020-05-18 22:11:49 -0300

    Implementado Class Department e Programa

---
+++ src/application/Programa.java
@@ -0,0 +1,13 @@
+package application;
+
+import model.entities.Department;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		Department dp = new Department(1,"livros");
+		System.out.println(dp);
+
+	}
+
+}
