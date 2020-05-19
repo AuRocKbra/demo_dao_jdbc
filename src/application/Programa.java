d53450db8780ce16e944f4b1117a0a3dd48b72ba
AuRocKbra <4haurock@gamil.com>
2020-05-18 22:25:28 -0300

    Implementado classa Seller

--- src/application/Programa.java
+++ src/application/Programa.java
@@ -1,12 +1,16 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Programa {
 
 	public static void main(String[] args) {
 		Department dp = new Department(1,"livros");
-		System.out.println(dp);
+		Seller seller = new Seller(1, "Bob","bob@gmail.com",new Date(),3000.0, dp);
+		System.out.println(seller);
 
 	}
 
